d1a91c55550f3c45208b090463a78baa3e557766
0xShad3 <angelkalai2@yahoo.gr>
2020-09-25 20:07:37 +0300

    Added important functions automated the functionality of adding known MAC addresses to the local network logs.
    TODO: Write a bash script to daemonize the the tool and set it up (download the custom script etc...) , cover the Discord bot functionality maybe in a second file.

--- network-monitor.py
+++ network-monitor.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import os
+from datetime import date
 import nmap
 import sys
 import time
@@ -9,6 +9,7 @@ import discord
 from discord.ext.commands import CommandNotFound
 from discord.ext import commands
 from discord.ext.commands import Cog,Context
+from threading import Timer
 
 discordBotAPIKey = "REDACTED"
 
@@ -24,28 +25,59 @@ except:
 	sys.exit(0)
 
 
-hostList = []
+
 
 @bot.command()
 async def displayUnkownDevices(ctx):
 	#will display all Uknown Devices which the discord user will name them 
+	print("Nothing")
 
 @bot.command()
 async def displayKnownDevices(ctx):
 	#will display connected devices and log last connections
+	print("Nothing")
+
+
+def fillUknownMACAddress(uknownHost):
+	timeout = 30
+	t = Timer(timeout,print,['[+] Time\'s up to fill the name about of this addresses'])
+	print("You have {} seconds to fill the name of this MAC address".format(timeout))
+	t.start()
+	toName = input()
+	t.cancel()
+	if toName is not None:
+		uknownHost[0] = toName
+		writeCommonMACAddress(uknownHost)
+		# now its a known host
+		return uknownHost
+
 
 def getCurrentTime():
 	now = datetime.now()
 	return now.strftime("%H:%M:%S")
 	
 ## Needs fix
-def writeCommonMACAddresses():
-	with open('mac_addresses.csv', 'w+',newline='') as csvfile:
+def writeCommonMACAddress(registry):
+	with open('mac_addresses.csv', 'a+',newline='') as csvfile:
 		writer = csv.writer(csvfile)
-		writer.writerow(hostList)
-		csvfile.close()
+		writer.writerow(registry)
 
-def scanNetwork():
+
+def replaceCommonMACAddresses(hostList):
+
+	with open('mac_addresses.csv','r') as csvfile:
+		reader = csv.reader(csvfile,skipinitialspace=True)
+
+		for row in reader:
+		
+			for host in hostList:
+			
+				if (host[1] == row[0]):
+					host[0] = row[1]
+	return hostList
+
+
+def scanNetwork(hostList):
 	nmp.scan(hosts='192.168.1.0/24',arguments='-script smb-os-discovery.nse -T5 192.168.1.0/24')
 	i = 0
 	for host in nmp.all_hosts():
@@ -54,39 +86,62 @@ def scanNetwork():
 			mac_adress = nmp[host].get('addresses').get('mac')
 			localIPv4 = nmp[host].get('addresses').get('ipv4')
 			vendor = nmp[host].get('vendor').get(mac_adress)
-
-			hostList.insert(i,[name,mac_adress,localIPv4,vendor])
+			hostList.append([name,mac_adress,localIPv4,vendor])
 			i += 1
 		except:
 			vendor = mac_adress = localIPv4 = 'unknown'	
 
+	return replaceCommonMACAddresses(hostList)
 	
+def keepLogs(logs):
+	with open(date.today().strftime("%d_%m_%y") + ".log","a+") as logfile:
+		logfile.write(logs)
+		logfile.close()
+
 def alert():
 	print("Alert")
 
 
 
 def main():
+	hostList = []
 	hostCountNew = 0
 	hostCountOld = 0
+	logs = ""
 	while True:
 
 		print("[+] Performing a NMAP scan ====> {}".format(getCurrentTime()))
-		scanNetwork()
+		logs += "[+] Performing a NMAP scan ====> {}\n".format(getCurrentTime())
+		hostList = scanNetwork(hostList)
+		for host in hostList:
+			if host[0] == "":
+				replIndex = hostList.index(host)
+				hostList[replIndex] = fillUknownMACAddress(host)
+
+
 		print("[+] Hosts found ===> {} ".format(len(hostList)))
+		logs += "[+] Hosts found ===> {} \n".format(len(hostList))
 
 		for host in hostList:
-			print("(*) Name -> {} MAC -> {} IPv4 -> {} Vendor -> {} ".format(host[0],host[1],host[2],host[3]))
+			if (host[0] != ""):
+				print("		:+: Name -> {} MAC -> {} IPv4 -> {} Vendor -> {} ".format(host[0],host[1],host[2],host[3]))
+				logs += "         :+: Name -> {} MAC -> {} IPv4 -> {} Vendor -> {} \n".format(host[0],host[1],host[2],host[3])
+			else:
+				print("		:+: Name -> [UNKNOWN] MAC -> {} IPv4 -> {} Vendor -> {} ".format(host[0],host[1],host[2],host[3]))
+				logs += "         :+: Name -> [UNKNOWN] MAC -> {} IPv4 -> {} Vendor -> {} \n".format(host[0],host[1],host[2],host[3])
 		
 		hostCountOld = hostCountNew
 		hostCountNew = len(hostList)
 		
 		if (hostCountNew > hostCountOld):
-			print("[A] Alerting owner new host connected".format(hostList[0]))
-			time.sleep(10)
-		time.sleep(300)
+			print("[ALERT] Alerting owner new host connected")
+			logs += "[ALERT] Alerting owner new host connected\n"
+			time.sleep(1)
 
+		keepLogs(logs)
+		time.sleep(300)
+		hostList.clear()
 
 if __name__ == '__main__':
 	main()
-	bot.run(discordBotAPIKey)
+	#bot.run(discordBotAPIKey)
